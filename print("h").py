286319bef46d2787cd6a6cf2c6db02c25352c6e0
mohammed herrag <85641709+Herrag-Mohammed@users.noreply.github.com>
2023-02-19 21:55:24 +0100

    Update print("h").py

--- print("h").py
+++ print("h").py
@@ -3,32 +3,35 @@ import rpa as r
 import pandas as pd
 import streamlit as st
 #read the excel file and store in dataframe variable df
-if st.button('Run >'):
-    df= pd.read_excel('challenge.xlsx')
+def run():
+    if st.button('Run >'):
+        df= pd.read_excel('challenge.xlsx')
 
-    #start the tagUI process
-    r.init()
+        #start the tagUI process
+        r.init()
 
-    #open the website
-    r.url('http://www.rpachallenge.com/')
-    r.wait(10)
-    #click on start button
-    r.click('//button[text()="Start"]')
+        #open the website
+        r.url('http://www.rpachallenge.com/')
+        r.wait(10)
+        #click on start button
+        r.click('//button[text()="Start"]')
 
-    #data entry operations
-    for index,row in df.iterrows():
-        r.type('//input[@ng-reflect-name="labelFirstName"]',row['First Name'])
-        r.type('//input[@ng-reflect-name="labelLastName"]',row['Last Name '])
-        r.type('//input[@ng-reflect-name="labelCompanyName"]',row['Company Name'])
-        r.type('//input[@ng-reflect-name="labelRole"]',row['Role in Company'])
-        r.type('//input[@ng-reflect-name="labelAddress"]',row['Address'])
-        r.type('//input[@ng-reflect-name="labelEmail"]',row['Email'])
-        r.type('//input[@ng-reflect-name="labelPhone"]',str(row['Phone Number']))
-        r.click('//input[@value="Submit"]')
+        #data entry operations
+        for index,row in df.iterrows():
+            r.type('//input[@ng-reflect-name="labelFirstName"]',row['First Name'])
+            r.type('//input[@ng-reflect-name="labelLastName"]',row['Last Name '])
+            r.type('//input[@ng-reflect-name="labelCompanyName"]',row['Company Name'])
+            r.type('//input[@ng-reflect-name="labelRole"]',row['Role in Company'])
+            r.type('//input[@ng-reflect-name="labelAddress"]',row['Address'])
+            r.type('//input[@ng-reflect-name="labelEmail"]',row['Email'])
+            r.type('//input[@ng-reflect-name="labelPhone"]',str(row['Phone Number']))
+            r.click('//input[@value="Submit"]')
 
-    #screenshot of webpage
+        #screenshot of webpage
 
-    r.snap('/html/body/app-root/div[2]','results.png')
+        r.snap('/html/body/app-root/div[2]','results.png')
 
-    #stop the tagui process
-    r.close()
+        #stop the tagui process
+        r.close()
+if __name__ == '__main__':
+    run()
